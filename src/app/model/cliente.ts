8b685fbc8a6d4ab78850e4cfe1a6439d30fa1fe9
AlessandroCristiano <alessandrocristiano66@gmail.com>
2023-01-03 13:18:08 +0100

    Creazione model

---
+++ src/app/model/cliente.ts
@@ -0,0 +1,7 @@
+export interface Cliente{
+    id?:number;
+    nome?:String;
+    cognome?:string;
+    indirizzo?:string;
+    attivo?:boolean;
+}
---
+++ src/app/model/user.ts
@@ -0,0 +1,10 @@
+export interface User{
+    id?:number;
+    nome?:string;
+    cognome?:string;
+    username?:string;
+    password?:string;
+    token?: string;
+    ruoli?: string[];
+
+}
